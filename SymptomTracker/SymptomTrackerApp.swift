361c40146a7a093cad7d341d504c4e0537aa62fa
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2022-02-13 18:38:17 -0500

    Initial Commit

---
+++ SymptomTracker/SymptomTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  SymptomTrackerApp.swift
+//  SymptomTracker
+//
+//  Created by Christopher Yoon on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SymptomTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
